f19d938de91805ae3a627f57d909e5d40b3eb1ad
cjw <449031389@qq.com>
2023-06-04 22:46:01 +0800

    add:提交最初文件

---
+++ src/main/java/com/cjw/iii/springlearn/SpringLearnApplication.java
@@ -0,0 +1,32 @@
+package com.cjw.iii.springlearn;
+
+import com.cjw.iii.springlearn.dto.OrderService;
+import com.cjw.iii.springlearn.dto.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.lang.reflect.Field;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("spring.xml");
+
+		AnnotationConfigApplicationContext annotationConfigApplicationContext= new AnnotationConfigApplicationContext(UserService.class);
+
+		UserService userService = annotationConfigApplicationContext.getBean(UserService.class);
+
+
+		for (Field field:userService.getClass().getFields()){
+			if(field.isAnnotationPresent(Autowired.class)){
+				//给该对象的该属性赋值
+//				field.set(userService,new OrderService());
+			}
+		}
+	}
+}
---
+++ src/main/java/com/cjw/iii/springlearn/dto/OrderService.java
@@ -0,0 +1,7 @@
+package com.cjw.iii.springlearn.dto;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderService {
+}
